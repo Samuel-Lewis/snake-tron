0e9eaa481f91ae43cc67abcc4436e345d4f89d03
Samuel Lewis <mazuel@gmail.com>
2024-06-27 15:25:42 +1000

    blank target, please build

--- src/pages/parts/Footer.tsx
+++ src/pages/parts/Footer.tsx
@@ -6,7 +6,10 @@ const { Footer } = Layout;
 export const FooterPart: React.FunctionComponent = () => {
   return (
     <Footer className="footer">
-      Created by <a href="http://samuel-lewis.com/">Samuel Lewis</a>
+      Created by{" "}
+      <a href="http://samuel-lewis.com/" target="_blank" rel="noreferrer">
+        Samuel Lewis
+      </a>
     </Footer>
   );
 };
